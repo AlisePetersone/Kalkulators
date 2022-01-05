3cbff46646b5943d0ae8344af888d129e26ecdfe
AlisePetersone <95718878+AlisePetersone@users.noreply.github.com>
2022-01-05 12:39:04 +0200

    new

--- src/com/AP/Main.java
+++ src/com/AP/Main.java
@@ -1,41 +1,30 @@
 package com.AP;
-
 import java.util.Scanner;
 import static java.lang.System.out;
 public class Main {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         //1. skaitlis
-
         out.println("1.skaitlis:");
-//        out.println("");
         int x = scanner.nextInt();
 
         //darbība
-        out.println("darbība:");
-//        out.println("");
+        out.println("Darbība:");
         String oper;
         scanner.nextLine();//nepieciešams,jo nextint nolasa string automātiski, bez ievades iespējas.
         oper = scanner.nextLine();
 
-
         //2. skaitlis
         out.println("2.skaitlis:");
-
-//        System.out.println("");
         int y = scanner.nextInt();
 
-
-
+        //rezultats
         out.println("Rezultāts:");
         if(oper.equals("+")) out.println(x + y);
         if(oper.equals("-")) out.println(x - y);
         if(oper.equals("*")) out.println(x * y);
         if(oper.equals("/")) out.println(x / y);
-
     }
 }
 

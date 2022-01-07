f05c67f91437bb46518945d10583a484cab0635d
AlisePetersone <95718878+AlisePetersone@users.noreply.github.com>
2022-01-07 18:34:31 +0200

    final

--- src/com/AP/Main.java
+++ src/com/AP/Main.java
@@ -3,28 +3,41 @@ import java.util.Scanner;
 import static java.lang.System.out;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         //1. skaitlis
-        out.println("1.skaitlis:");
-        int x = scanner.nextInt();
+        out.println("1. skaitlis:");
+        while (!sc.hasNextInt()) {
+            out.println("SKAITLI!!!:");
+            sc.next();}
+        int x = sc.nextInt();
 
         //darbība
-        out.println("Darbība:");
-        String oper;
-        scanner.nextLine();//nepieciešams,jo nextint nolasa string automātiski, bez ievades iespējas.
-        oper = scanner.nextLine();
-
+        boolean darbiba = true;
+        String oper = null; // oper piešķīru 0 vērtību, jo switch met kļūdu. Kodā oper vērtība tiks piešķirta pēc ievades.
+        while (darbiba) {
+            out.println("Ievadiet '/', '*', '+', '-'  darbību:");
+            sc.nextLine();//nepieciešams,jo nextint nolasa string automātiski, bez ievades iespējas.
+            oper = sc.nextLine();
+            if (oper.equals("+") || oper.equals("-") || oper.equals("/") || oper.equals("*")) {
+                darbiba = false;
+            }
+        }
         //2. skaitlis
-        out.println("2.skaitlis:");
-        int y = scanner.nextInt();
+        out.println("2. skaitlis:");
+        while (!sc.hasNextInt()) {
+            out.println("Veselu skaitli!!!:");
+            sc.next();
+        }
+        int y = sc.nextInt();
 
-        //rezultats
         out.println("Rezultāts:");
-        if(oper.equals("+")) out.println(x + y);
-        if(oper.equals("-")) out.println(x - y);
-        if(oper.equals("*")) out.println(x * y);
-        if(oper.equals("/")) out.println(x / y);
+        switch (oper) {
+            case "+" -> out.println(x + y);
+            case "-" -> out.println(x - y);
+            case "*" -> out.println(x * y);
+            case "/" -> out.println(x / y);
+        }
     }
 }
 
